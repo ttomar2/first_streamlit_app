813583b5bfa78dafdd31c285832b96b7a744fec2
ttomar2 <112103040+ttomar2@users.noreply.github.com>
2022-08-25 22:54:17 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,1 +1,10 @@
+import Streamlit
 
+Streamlit.title('my parent healthy denner')
+
+
+Streamlit.header('🥣 Breakfast Menu')
+Streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+Streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+Streamlit.text('🥑 Hard-Boiled Free-Range Egg')
+Streamlit.text('🍞 Avacado Toast')
